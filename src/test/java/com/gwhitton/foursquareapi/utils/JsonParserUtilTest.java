6fb459c15d4a64364d047d7cf53722b379dcfd2c
Gordon Whitton <gordonrobertwhitton@gmail.com>
2017-07-20 10:12:14 +0100

    test json parser (failing)

--- src/test/java/com/gwhitton/foursquareapi/utils/JsonParserUtilTest.java
+++ src/test/java/com/gwhitton/foursquareapi/utils/JsonParserUtilTest.java
@@ -2,13 +2,22 @@ package com.gwhitton.foursquareapi.utils;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 public class JsonParserUtilTest {
 
 	@Test
-	public void testGetValues() {
-		fail("Not yet implemented");
+	public void testGetValues() throws JasonParserUtilException {
+
+		String json = "{\"response\": {\"venues\": [{\"name\": \"name1\"},{\"name\": \"name2\"},{\"name\": \"name3\"}]}}";
+
+		List<String> result = JsonParserUtil.getValues(json, "venues", "name");
+
+		for(String s : result){
+			System.out.println(s);
+		}
 	}
 
 }
